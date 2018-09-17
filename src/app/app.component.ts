0f467448f404dd61201d7c54457adb0d98981e10
jamils82 <sjamils82@gmail.com>
2018-09-17 11:30:19 +0500

    ASDASDADAD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,8 +52,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit () {
     this.getCat();
-    console.log(this.token);
-    this.opentokService.gettoken(JSON.stringify(this.token));
   }
   errorHandler(err) {
     if (err && err.message) {
@@ -100,6 +98,7 @@ export class AppComponent implements OnInit {
     });
   }
   hidediv() {
+    this.opentokService.gettoken(this.token);
     // this.route.navigate(['/doctorb']);
     this.wel = !this.wel;
     this.call = true;
