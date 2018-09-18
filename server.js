c70a262eae82c4545c8db247cd242c6261c2fa77
jamils82 <sjamils82@gmail.com>
2018-09-19 00:07:49 +0500

    ASDASD

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(function (req, res, next) {
       name: a.name
     })
   })
-  app.route('/api/session/').get((req, res) => {
+  app.route('/api/cat/:name').get((req, res) => {
     const requestedCatName = req.params['name'];
 
     res.send(myname);
