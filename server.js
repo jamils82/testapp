b1e83ab01b5e47e67b9771989201e2be79a6c4cb
jamils82 <sjamils82@gmail.com>
2018-09-18 13:04:06 +0500

    adadaD

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use(function (req, res, next) {
   });
   app.route('/api/session/:name').get((req, res) => {
     const requestedCatName = req.params['name'];
-    res.send({ name: requestedCatName });
+    res.send( requestedCatName );
   });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000 , function () {
