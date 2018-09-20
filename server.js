65dccb1910244d009104278310fd3b5ce7b5cd59
jamils82 <sjamils82@gmail.com>
2018-09-20 09:44:03 +0500

    ASDAD

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const myname ='saad';
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
   const apiKey=  '46168292';
+  var callernames = [];
   var requestedCatName ='hadi';
   const apiSecret = '828124981dd61607ed239dcc30838cebcf5daebd';
   opentok = new OpenTok(apiKey, apiSecret);
@@ -61,11 +62,11 @@ app.use(function (req, res, next) {
  
   app.route('/api/cats/:name').get((req, res) => {
     requestedCatName = req.params['name'];
-    res.send({ requestedCatName });
+    res.send( requestedCatName );
   });
   
   app.route('/api/sess').get((req, res ) => {
-    res.send({requestedCatName});
+    res.send(requestedCatName);
   })
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000 , function () {
