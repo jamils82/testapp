f177e35e74c022c9d03bfa883e10beef7e7e9cdd
jamils82 <sjamils82@gmail.com>
2018-09-22 20:03:16 +0500

    saad

--- src/app/doctora/doctora.component.ts
+++ src/app/doctora/doctora.component.ts
@@ -74,7 +74,7 @@ export class DoctoraComponent implements OnInit {
   getname() {
     return this.http.get('https://doctestapp.herokuapp.com/api/sess' ).subscribe( data => {
       this.onlinecallers = JSON.stringify(data);
-      this.callerName = JSON.stringify(this.callerName);
+     // this.callerName = JSON.stringify(this.callerName);
       console.log(this.onlinecallers);
     });
   }
