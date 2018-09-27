a4aaeea5e814bad5b3eadc54a991ebe9e2330223
jamils82 <sjamils82@gmail.com>
2018-09-27 07:15:22 +0500

    Saad

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -112,9 +112,7 @@ export class PatientComponent implements OnInit {
     this.getSess(this.callername);
     this.wel = !this.wel;
     this.call = true;
-    if (this.favcaller ) {
     this.end = true;
-    }
     this.connectcall();
    }
    endcall() {
