9be4f728c3207b3bdc98ef88c0bd45b8113b80d9
jamils82 <sjamils82@gmail.com>
2018-09-27 05:25:05 +0500

    Saad

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -148,6 +148,8 @@ export class PatientComponent implements OnInit {
       console.error(err);
       alert('Unable to connect. Make sure you have Internet Working.');
     });
+    } else {
+      console.log('looking');
     }
   }
    publish() {
