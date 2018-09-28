176ee157625aff9fdbd8f840cb16240062e7cfa8
jamils82 <sjamils82@gmail.com>
2018-09-28 12:28:54 +0500

    saad

--- src/app/doctora/doctora.component.ts
+++ src/app/doctora/doctora.component.ts
@@ -182,9 +182,8 @@ export class DoctoraComponent implements OnInit {
     .catch(console.log);*/
   }
   endcall() {
-    this.session.unpublish(this.publisher);
     this.session.disconnect();
-    this.session.unpublish(this.publisher);
+    this.ngOnInit();
     this.list = !this.list;
     this.end = false;
     this.wel = !this.wel;
