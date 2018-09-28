eecd813b0f9630643db6aa8e681ca0b9587db466
jamils82 <sjamils82@gmail.com>
2018-09-28 14:42:54 +0500

    saad

--- src/app/doctora/doctora.component.ts
+++ src/app/doctora/doctora.component.ts
@@ -105,10 +105,17 @@ export class DoctoraComponent implements OnInit {
       console.log('connnected to session');
       const ot = this.opentokService.getOT();
         this.pubdiv = document.getElementById('pubvideo');
-        this.session.connect();
+
+        if (this.session) {
+          if (this.session['isConnected']()) {
+            if (this.publisher) {
+              this.session.unpublish(this.publisher);
+            }
             this.publisher = ot.initPublisher(this.pubdiv, {insertMode: 'append', width : '100%', height : '100%'});
             this.publish();
+          }
           this.session.on('sessionConnected', () => this.publish());
+      }
       this.session.on('streamDestroyed', (event) => {
         const idx = this.streams.indexOf(event.stream);
         if (idx > -1) {
