4adf6731c36b399dddf7ee6f5aa5ed24d53d9061
jamils82 <sjamils82@gmail.com>
2018-10-03 10:53:55 +0500

    multiple

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('', function(req,res) {
    
   res.sendFile(path.join(__dirname+'/dist/testapp/index.html'));
   });
-  app.get('/*', function(req,res) {
+  app.get('*', function(req,res) {
    
     res.sendFile(path.join(__dirname+'/dist/testapp/index.html'));
     });
