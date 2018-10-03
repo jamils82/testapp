e7d5a93405410ebbb491cf418fd69b1c3c4b59db
jamils82 <sjamils82@gmail.com>
2018-10-03 23:09:34 +0500

    saad

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.listen(process.env.PORT || 5000 , function () {
     console.log(process.env.PORT || 5000);
 } );
 
-app.get('/*', function(req,res) {
+app.get('/', function(req,res) {
     
   res.sendFile(path.join(__dirname+'/dist/testapp/index.html'));
   });
