9e01cad9849eaad6eb67c789480ea56b2e18e8d5
jamils82 <sjamils82@gmail.com>
2018-10-03 11:04:13 +0500

    multiple

--- server.js
+++ server.js
@@ -26,10 +26,6 @@ app.get('', function(req,res) {
    
   res.sendFile(path.join(__dirname+'/dist/testapp/index.html'));
   });
-  app.get('*', function(req,res) {
-   
-    res.sendFile(path.join(__dirname+'/dist/testapp/index.html'));
-    });
 var jsonParser = bodyParser.json();
 app.use(jsonParser);
 app.use(bodyParser.urlencoded({ extended: false }))
