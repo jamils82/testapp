5c6410c2beb8af31c8c7b7e493d9d29001ea8f63
jamils82 <sjamils82@gmail.com>
2018-10-03 23:00:10 +0500

    saad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,93 +18,12 @@ export interface Cat {
   providers: [ OpentokService ]
 })
 export class AppComponent implements OnInit {
-  closeResult: string;
-  title = 'Angular Basic Video Chat';
-  session: OT.Session;
-  token = '123';
-  streams: Array<OT.Stream> = [];
-  changeDetectorRef: ChangeDetectorRef;
-  wel = true;
-  end = false;
-  config: any;
-  call = false;
-  onHold = false;
-    subscriberOpts: {
-      insertMode: 'append',
-      width: '100%',
-      height: '100%'
-    };
+
      constructor(private ref: ChangeDetectorRef, private opentokService: OpentokService, private http: HttpClient , private route: Router) {
-    this.changeDetectorRef = ref;
-  }
+   }
 
   ngOnInit () {
    // this.getCat();
-   this.route.navigate(['/doctor']);
-  }
-  errorHandler(err) {
-    if (err && err.message) {
-     console.log(err);
-      // UIkit.notification(err.message, { pos: 'bottom-left', status: 'danger' })
-    }
-  }
-  getCat() {
-    return this.http.get('https://doctestapp.herokuapp.com/api/cat', {responseType: 'text'}).subscribe( data => {
-      // console.log(data);
-      // this.token = data;
-      // alert(this.token);
-      console.log(this.token);
-    });
-  }
-  hidediv() {
-   // this.opentokService.gettoken(this.token);
-     this.route.navigate(['/doctorb']);
-   // console.log(JSON.stringify(this.token));
-    this.wel = !this.wel;
-    this.call = true;
-    this.end = true;
-    this.opentokService.initSession().then((session: OT.Session) => {
-      this.session = session;
-      this.session.on('streamCreated', (event) => {
-        console.log(session);
-        this.streams.push(event.stream);
-        this.changeDetectorRef.detectChanges();
-      });
-      this.session.on('streamDestroyed', (event) => {
-        const idx = this.streams.indexOf(event.stream);
-        if (idx > -1) {
-          this.streams.splice(idx, 1);
-          this.changeDetectorRef.detectChanges();
-        }
-      });
-    })
-    .then(() => this.opentokService.connect())
-    .catch((err) => {
-      console.error(err);
-      alert('Unable to connect. Make sure you have Internet Working.');
-    });
-  }
-  onSubmit() {
-   /* // tslint:disable-next-line:max-line-length
-    axios.post('http://localhost:8000/dial' )
-    .then(res => {
-      this.caller = res.data.caller;
-      this.otConnect(res.data.apiKey, res.data.caller.sessionId, res.data.caller.token);
-    })
-    .catch(console.log);*/
-  }
-  endcall() {
-    this.session.disconnect();
-    this.end = false;
-    this.wel = !this.wel;
-    this.call = !this.call;
-  }
-  doctor() {
-    this.wel = !this.wel;
-    this.route.navigate(['/doctora']);
-  }
-  patient() {
-    this.wel = !this.wel;
-    this.route.navigate(['/doctorb']);
+   this.route.navigate(['/mydoctor']);
   }
 }
