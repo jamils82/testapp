8c7db6ca4e0571285c58c06b4ad4ba1627204cf0
jamils82 <sjamils82@gmail.com>
2018-10-04 07:22:53 +0500

    saad

--- server.js
+++ server.js
@@ -98,8 +98,7 @@ app.get('/*', function(req,res) {
     
   res.sendFile(path.join(__dirname+'/dist/testapp/index.html'));
   });
-  
-app.get('', function(req,res) {
+  app.get('', function(req,res) {
      
-  res.sendFile(path.join(__dirname+'/dist/testapp/index.html'));
-});
+    res.sendFile(path.join(__dirname+'/dist/testapp/index.html'));
+   });
