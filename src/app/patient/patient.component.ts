37ff750971041ca875c76292815dff28e2a76d5d
jamils82 <sjamils82@gmail.com>
2018-10-06 14:43:15 +0500

    saad

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -50,7 +50,7 @@ export class PatientComponent implements OnInit {
   pname: string;
   // tslint:disable-next-line:max-line-length
   constructor(private ref: ChangeDetectorRef,  public activatedRoute: ActivatedRoute,  private http: HttpClient, private opentokService: OpentokService, private route: Router ) {
-    this.socket = io();
+    this.socket = io('http://doctestapp.herokuapp.com');
    }
 
   ngOnInit() {
