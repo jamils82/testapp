906f9f7424c2530fdf2c9f4bea83d112782204b5
jamils82 <sjamils82@gmail.com>
2018-10-06 19:03:08 +0500

    saad

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const myname ='saad';
 const bodyParser = require('body-parser');
 
 var server =http.createServer(app);
-var io = SocketIO(server);
+var io = socketIO(server);
 app.use(bodyParser.json());
 app.use(cors());
   const apiKey=  '46168292';
@@ -70,6 +70,62 @@ app.use(function (req, res, next) {
   // this message will NOT go to the client defined above
   io.sockets.in('foobar').emit('message', 'anyone in this room yet?');
   
+  app.route('/api/cats').get((req, res) => {
+    res.send({
+      cats: [{ name: 'lilly' }, { name: 'lucy' }]
+    });
+  });
+  
+  
+  app.route('/api/pattok' ).get((req, res) => {
+    const token = opentok.generateToken(SESSION_ID, {
+      role: 'subscriber'});
+    res.send( token );
+  });
+  app.route('/api/cats',jsonParser ).post((req, res) => {
+    if (!req.body) return res.sendStatus(400)
+    // myname = req.body.params.name;
+    res.send(201, req.body);
+  });
+ 
+  app.route('/api/cats/:name').get((req, res) => {
+    requestedCatName = req.params['name'];
+    newLength = callernames.push(req.params['name']);
+   
+    res.send( {requestedCatName} );
+  });
+  
+  app.route('/api/patobj/:name').get((req, res) => {
+    requestedCatName = req.params['name'];
+    });
+  app.route('/api/docobj/:name').get((req, res) => {
+    res.send({requestedCatName} );
+  });
+  app.route('/api/connecteddoctor/:bool').get((req, res) => {
+    doctorconnected = true;
+   // newLength = callernames.push(req.params['name']);
+    res.send( doctorconnected );
+  });
+  app.route('/api/connecteddoctor').get((req, res) => {
+    res.send( doctorconnected );
+  });
+  app.route('/api/favcaller/:name').get((req,res) => {
+    favcaller = req.params['name'];
+  });
+  app.route('/api/getfavcaller').get((req,res) => {
+    res.send(favcaller);
+  } );
+  app.route('/api/sess').get((req, res ) => {
+    res.send(callernames);
+  })
+  app.route('/api/session/:name').get((req, res) => {
+    requestedCatName = req.params['name'];
+    var a = callernames.indexOf(requestedCatName);
+    if(a > -1)
+      callernames.splice(a,1); 
+   // newLength =callernames.pop(requestedCatName);
+    res.send( {requestedCatName} );
+  });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000 , function () {
     console.log(process.env.PORT || 5000);
