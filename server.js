2b3cdb823b6012b10d989a9f24c2c3e6d45c8090
jamils82 <sjamils82@gmail.com>
2018-10-07 19:56:13 +0500

    socket

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.use(function (req, res, next) {
     res.send( {requestedCatName} );
   });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000 , function () {
+server.listen(process.env.PORT || 5000 , function () {
     console.log(process.env.PORT || 5000);
 } );
 
