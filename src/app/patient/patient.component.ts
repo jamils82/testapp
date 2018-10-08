c2448610266c066838546f79f632ece7e3fc5c92
jamils82 <sjamils82@gmail.com>
2018-10-08 07:35:40 +0500

    saad

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -75,10 +75,10 @@ export class PatientComponent implements OnInit {
         console.log(msg);
         console.log(this.messages);
     });
-    this.socket.emit('join-room',  {room : this.pname } );
+    this.socket.emit('join-room', {room : this.pname } );
     this.socket.emit('add-user' , {username : this.callername } );
     console.log(this.pname);
-    this.socket.emit('request-users', {});
+    this.socket.on('request-users', {});
     this.socket.on('users', (data) => {
       this.onlineusers = data.users;
     } );
