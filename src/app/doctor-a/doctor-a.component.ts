ecedaf21b955fecb05c2bf59f4f2a78ece21fbd5
jamils82 <sjamils82@gmail.com>
2018-10-15 16:24:16 +0500

    socket

--- src/app/doctor-a/doctor-a.component.ts
+++ src/app/doctor-a/doctor-a.component.ts
@@ -62,7 +62,10 @@ export class DoctorAComponent implements OnInit {
     });
     this.sendroom(this.room);
     this.senduser('');
-
+    this.sendMessage('');
+     setInterval( () => {
+       this.sendMessage('');
+     } , 7000 );
     this.postconnect();
     setInterval(() => {
     this.getname().subscribe( data => {
