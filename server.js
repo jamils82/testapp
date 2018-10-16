ec89b6a4830e37eb0afd00517354c33faa04ade6
jamils82 <sjamils82@gmail.com>
2018-10-16 12:02:38 +0500

    Sock

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ io.on('connection', function(socket) {
 
 socket.on('disconnect', function(data){
   console.log(username + ' has disconnected');
-  users.splice(connectedusersusers.indexOf(username), 1);
+  users.splice(connectedusers.indexOf(username), 1);
   io.to(room).emit('remove-user', {username: username});
 });
 });
