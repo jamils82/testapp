3f75d9511891536ab6f4a1a1e926c06d2345dd99
jamils82 <sjamils82@gmail.com>
2018-10-22 13:40:15 +0500

    sadasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,7 +38,7 @@ import {appRoutes} from './routes';
     MatListModule,
     RouterModule.forRoot(
       appRoutes,
-   //  { useHash: true }
+     { useHash: true }
     // {enableTracing: true}
     ),
     HttpClientModule
