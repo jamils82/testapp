d87aaf28474c97880972ff8d027b077a50156019
jamils82 <sjamils82@gmail.com>
2018-10-23 12:09:07 +0500

    sasda

--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -6,8 +6,6 @@ import { catchError, map, tap } from 'rxjs/operators';
 import {  RouterModule, Routes, Router , ActivatedRoute } from '@angular/router';
 import * as io from 'socket.io-client';
 import { Observable } from 'rxjs';
-import { NgModule } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
 declare var OT: any;
 
 const publish = () => {
@@ -73,8 +71,6 @@ export class PatientComponent implements OnInit {
   pname: string;
   vid: boolean;
   myroom: string;
-  heroForm: any;
-  hero: any;
   // tslint:disable-next-line:max-line-length
   constructor(private ref: ChangeDetectorRef,  public activatedRoute: ActivatedRoute,  private http: HttpClient, private opentokService: OpentokService, private route: Router ) {
     this.socket = io.connect('https://doctestapp.herokuapp.com');
@@ -93,14 +89,6 @@ export class PatientComponent implements OnInit {
       browser.webkitGetUserMedia ||
       browser.mozGetUserMedia ||
       browser.msGetUserMedia);
-      this.heroForm = new FormGroup({
-        'name': new FormControl(this.hero.name, [
-          Validators.required,
-          Validators.minLength(4),
-        ]),
-        'alterEgo': new FormControl(this.hero.alterEgo),
-        'power': new FormControl(this.hero.power, Validators.required)
-      });
 
    // browser.mediaDevices.getUserMedia({ video: true, audio: true });
      if (this.room  === 'DoctorA' ) {
@@ -221,7 +209,6 @@ export class PatientComponent implements OnInit {
   }
 
   hidediv(name: string , phone: string  ) {
-
     this.callername = name;
     const myph = phone;
     this.socket.emit('sendroom' , this.room );
